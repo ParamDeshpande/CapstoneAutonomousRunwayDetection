c406c9f356c21d77b980081b8df506c45cb82571
ParamDeshpande <deshpandeparam10@gmail.com>
2020-10-22 15:32:06 +0530

    messed up dont bother looking

--- src/rsa.c
+++ src/rsa.c
@@ -4,8 +4,8 @@
 #include<string.h>
 
 long int p,q,n,t,flag,e[100],d[100],temp[100],j,m[100],en[100],i;
-char msg[100];
-int prime(long int);
+
+
 void ce();
 long int cd(long int);
 void encrypt();
@@ -16,28 +16,6 @@ void decrypt();
 #ifdef USE_RSA_MAIN
 void main()
 {
-
-    printf("\nENTER FIRST PRIME NUMBER\n");
-    scanf("%d",&p);
-    flag=prime(p);
-    if(flag==0)
-    {
-        printf("\nWRONG INPUT\n");
-        exit(1);
-    }
-    printf("\nENTER ANOTHER PRIME NUMBER\n");
-    scanf("%d",&q);
-    flag=prime(q);
-    if(flag==0||p==q)
-    {
-        printf("\nWRONG INPUT\n");
-        exit(1);
-    }
-    printf("\nENTER MESSAGE\n");
-    fflush(stdin);
-    scanf("%s",msg);
-    for(i=0;msg[i]!=NULL;i++)
-        m[i]=msg[i];
     n=p*q;
     t=(p-1)*(q-1);
     ce();
@@ -99,26 +77,14 @@ void encrypt()
 {
     long int pt,ct,key=e[0],k,len;
     i=0;
-    len=strlen(msg);
-    while(i!=len)
-    {
-        pt=m[i];
-        pt=pt-96;
         k=1;
         for(j=0;j<key;j++)
         {
             k=k*pt;
             k=k%n;
         }
-        temp[i]=k;
-        ct=k+96;
-        en[i]=ct;
         i++;
     }
-    en[i]=-1;
-    printf("\nTHE ENCRYPTED MESSAGE IS\n");
-    for(i=0;en[i]!=-1;i++)
-        printf("%c",en[i]);
 }
 void decrypt()
 {
